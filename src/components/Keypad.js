5555cacbbdc9c98988d4133ada67a54a793dea90
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-01 18:05:10 +0300

    creates a password input and adds an eventlistener

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,20 @@
 // Code Keypad Component Here
+import React from "react";
+
+function handleChange (){
+  console.log("Entering password...")
+}
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+          <input
+           type="password"
+           placeholder="Enter password"
+           onChange ={handleChange}
+           name="password"
+           />
+        </div>
     )
 }
 
